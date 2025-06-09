bc96711a13860cc374352787414d72ae015046ac
Ibrahim Bouchaala <49410570+Maniseniler@users.noreply.github.com>
2025-06-09 09:46:16 +0100

    Add files via upload

---
+++ resources/MapHost/Music.lua
@@ -0,0 +1,8 @@
+﻿addEventHandler( "onClientResourceStart", getResourceRootElement( getThisResource()),
+	function()
+local desusound =playSound3D("Son.mp3",155.68431, -1953.26465, 3.77344, true)
+setSoundVolume(desusound,1.8)
+setSoundMaxDistance(desusound, 200)
+    end
+	           )
+
