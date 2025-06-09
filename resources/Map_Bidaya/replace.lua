ecceb69c5179b4ad14988e82b99d832c9dc68e32
Ibrahim Bouchaala <49410570+Maniseniler@users.noreply.github.com>
2025-06-09 09:48:26 +0100

    Add files via upload

---
+++ resources/Map_Bidaya/replace.lua
@@ -0,0 +1,11 @@
+txd1 = engineLoadTXD("Txd/6189.txd")
+engineImportTXD(txd1, 6189)
+
+txd2 = engineLoadTXD("Txd/3095.txd")
+engineImportTXD(txd2, 3095)
+
+txd3 = engineLoadTXD ( "Txd/Club.txd" )
+engineImportTXD(txd3, 3749)
+
+txd4 = engineLoadTXD("Txd/9191.txd")
+engineImportTXD(txd4, 9191)
