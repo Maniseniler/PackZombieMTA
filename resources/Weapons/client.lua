04c54649007ba4661c27dd4c0a849942d5ead75c
Ibrahim Bouchaala <49410570+Maniseniler@users.noreply.github.com>
2025-06-09 10:04:56 +0100

    Add files via upload

---
+++ resources/Weapons/client.lua
@@ -0,0 +1,310 @@
+﻿outputChatBox("#0077FFMod Weapon by Ibrahim Bouchaala",200,0,0,true)
+function centerWindow (center_window)
+    local screenW, screenH = guiGetScreenSize()
+    local windowW, windowH = guiGetSize(center_window, false)
+    local x, y = (screenW - windowW)/2,(screenH - windowH)/2
+    guiSetPosition(center_window, x, y, false)
+end
+
+jobpick = createPickup(2567.0576,-1280.4055,1044.1250,3,1)
+Sskin = createPed(292, 2567.0576,-1280.4055,1044.1250,180)
+setElementInterior(Sskin,2)
+setElementDimension(Sskin,5)
+setElementInterior(jobpick,2)
+setElementDimension(jobpick,5)
+
+function openJobWindow(source)
+	if source == localPlayer and getElementType(source) == "player" then
+		local job = getElementData(localPlayer,"worldjob")
+		if job == true then
+			if getElementData(source,"part") == false then
+				if getElementData(source,"weapon") == false then
+					guiSetVisible(jobquitwindow,true)
+					showCursor(true)
+				else
+					outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : لم تكمل المهمة",200,0,0,true)
+				end
+			else
+				outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : لم تكمل المهمة",200,0,0,true)
+			end
+		elseif job == false then
+			guiSetVisible(jobjoinwindow,true)
+			showCursor(true)
+		end
+	end
+end
+addEventHandler("onClientPickupHit",jobpick,openJobWindow)
+
+jobjoinwindow = guiCreateWindow(0,0,0.20,0.35,"لوحة التوظيف لمصنع الاسلحة المطور",true)
+guiSetProperty(jobjoinwindow, "CaptionColour", "FF0041FC")
+guiWindowSetMovable(jobjoinwindow,false)
+guiWindowSetSizable(jobjoinwindow,false)
+guiSetVisible(jobjoinwindow,false)
+centerWindow(jobjoinwindow)
+
+
+joinlabel = guiCreateMemo(0.02,0.1,1.2,0.65, "مود مصنع الاسلحة المطور                  \n\nالأن اصبح من السهل الاشتغال في المصنع       \n\n\nالقوانين :                                                             \n\nممنوع قتل أي شخص بالسلاح الذي يتم اعطائه          \n\nممنوع ضرب الطاولات و تكسيرهم                    \n\nملاحظات :                                                   \n                 \nيجب ارتداء قناع                                 \n\n مع تحيات صانع المود                       Ibrahim Bouchaala", true, jobjoinwindow)
+        guiMemoSetReadOnly(joinlabel, true)
+		guiSetFont(joinlabel, "default-bold-small")
+
+closejoinwindow = guiCreateButton(0.65,0.80,0.3,0.2,"إلغاء",true,jobjoinwindow)
+guiSetFont(closejoinwindow,"default-bold-small")
+guiSetProperty(closejoinwindow, "NormalTextColour", "FFFD1D00")
+
+joinjobbutton = guiCreateButton(0.05,0.80,0.3,0.2,"اخذ الوظيفة",true,jobjoinwindow)
+guiSetFont(joinjobbutton,"default-bold-small")
+guiSetProperty(joinjobbutton, "NormalTextColour", "FF05FB00")
+
+
+function closeJoinWindow(button)
+	if button == "left" then
+		if source == closejoinwindow then
+			if guiGetVisible(jobjoinwindow) == true then
+				guiSetVisible(jobjoinwindow,false)
+				showCursor(false)
+			end
+		end
+	end
+end
+addEventHandler("onClientGUIClick",closejoinwindow,closeJoinWindow)
+
+function joinJob(button)
+	if button == "left" then
+		if source == joinjobbutton then
+			setElementData(localPlayer,"worldjob",true)
+			toggleControl("fire",false)
+			toggleControl("sprint",false)
+			toggleControl("next_weapon",false)
+			toggleControl("previous_weapon",false)
+			outputChatBox("#FF9900* #FFFFFF[#0077FF Weapon #FFFFFF] : انت الأن موظف يجب عليك الحذر من الجيش",255,255,255,true)
+			guiSetVisible(jobjoinwindow,false)
+			showCursor(false)
+		end
+	end
+end
+addEventHandler("onClientGUIClick",joinjobbutton,joinJob)
+
+jobquitwindow = guiCreateWindow(0,0,0.3,0.1,"الخروج من الوظيفة",true)
+guiSetProperty(jobquitwindow, "CaptionColour", "FF0041FC")
+guiWindowSetMovable(jobquitwindow,false)
+guiWindowSetSizable(jobquitwindow,false)
+guiSetVisible(jobquitwindow,false)
+centerWindow(jobquitwindow)
+
+quitlabel = guiCreateLabel(0.1,0.325,0.9,0.5,"            حين تقوم بترك الوظيفة ستباع الاسلحة تلقائيا                             ",true,jobquitwindow)
+guiSetFont(quitlabel, "default-bold-small")
+
+closequitwindow = guiCreateButton(0.65,0.65,0.3,0.3,"إلغاء",true,jobquitwindow)
+guiSetFont(closequitwindow, "default-bold-small")
+guiSetProperty(closequitwindow, "NormalTextColour", "FF05FB00")
+
+quitjobbutton = guiCreateButton(0.05,0.65,0.3,0.3,"موافق",true,jobquitwindow)
+guiSetFont(quitjobbutton, "default-bold-small")
+guiSetProperty(quitjobbutton, "NormalTextColour", "FFFD1D00")
+
+function closeQuitWindow(button)
+	if button == "left" then
+		if source == closequitwindow then
+			if guiGetVisible(jobquitwindow) == true then
+				guiSetVisible(jobquitwindow,false)
+				showCursor(false)
+			end
+		end
+	end
+end
+addEventHandler("onClientGUIClick",closequitwindow,closeQuitWindow)
+
+function quitJob(button)
+	if button == "left" then
+		if source == quitjobbutton then
+			if getElementData(localPlayer,"worldjob") == true then
+				triggerServerEvent("onPlayerQuitJob",localPlayer,localPlayer)
+				triggerServerEvent("giveJobMoney",localPlayer,localPlayer)
+				guiSetVisible(jobquitwindow,false)
+				showCursor(false)
+			else
+				outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : !!!!!!!",200,0,0,true)
+			end
+		end
+	end
+end
+addEventHandler("onClientGUIClick",quitjobbutton,quitJob)
+
+cashpick = createPickup(2570.5767,-1280.2495,1044.1250,3,1)
+setElementInterior(cashpick,2)
+setElementDimension(cashpick,5)
+Sskin = createPed(292, 2570.5767,-1280.2495,1044.1250,180)
+setElementInterior(Sskin,2)
+setElementDimension(Sskin,5)
+
+cashwindow = guiCreateWindow(0,0,0.1,0.15,"البائع",true)
+guiWindowSetMovable(cashwindow,false)
+guiWindowSetSizable(cashwindow,false)
+guiSetVisible(cashwindow,false)
+centerWindow(cashwindow)
+
+cashlabel = guiCreateLabel(0.07,0.15,1,1,"",true,cashwindow)
+guiSetFont(cashlabel, "default-bold-small")
+
+takecashwindow = guiCreateButton(0.05,0.77,0.50,0.17,"بيع الاسلحة",true,cashwindow)
+guiSetFont(takecashwindow, "default-bold-small")
+guiSetProperty(takecashwindow, "NormalTextColour", "FF05FB00")
+
+closecashwindow = guiCreateButton(0.6,0.77,0.35,0.17,"إلغاء",true,cashwindow)
+guiSetFont(closecashwindow, "default-bold-small")
+guiSetProperty(closecashwindow, "NormalTextColour", "FFFD1D00")
+
+function openCashWindow(source)
+	if source == localPlayer and getElementType(source) == "player" then
+		if getElementData(source,"worldjob") == true then
+			if getElementData(source,"jobmoney") >= 20 then
+				if guiGetVisible(cashwindow) == false then
+					guiSetText ( cashlabel,"سعر الاسلحة المصنوعة ["..getElementData(localPlayer,"jobmoney").." $]")
+					guiSetVisible(cashwindow,true)
+					showCursor(true)
+				end
+			else
+				outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : انت لم تعمل اصلا",200,0,0,true)
+			end
+		else
+			outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : انت لا تعمل هنا",200,0,0,true)
+		end
+	end
+end
+addEventHandler("onClientPickupHit",cashpick,openCashWindow)
+
+function closeCashWindow(button)
+	if button == "left" then
+		if source == closecashwindow then
+			if guiGetVisible(cashwindow) == true then
+				guiSetVisible(cashwindow,false)
+				showCursor(false)
+			end
+		end
+	end
+end
+addEventHandler("onClientGUIClick",closecashwindow,closeCashWindow)
+
+function giveMeCash(button)
+	if button == "left" then
+		if source == takecashwindow then
+			triggerServerEvent("giveJobMoney",localPlayer,localPlayer)
+			guiSetVisible(cashwindow,false)
+			showCursor(false)
+		end
+	end
+end
+addEventHandler("onClientGUIClick",takecashwindow,giveMeCash)
+
+Works_Job_Enter = createPickup(-86.2715,-299.3637,2.7646,3,1318,10)
+--CreateBlipAttachedTo(Works_Job_Enter,47)
+
+function onClientLeaveEnterWorksJoblLSPickup(source)
+	if (source == localPlayer and getElementType(source) == "player" and not isPedInVehicle(source)) then
+		toggleControl("enter_exit",true)
+		unbindKey("F","down",WorksJobEnterDoor)
+	end
+end
+addEventHandler("onClientPickupLeave",Works_Job_Enter,onClientLeaveEnterWorksJoblLSPickup)
+
+function onClientHitEnterWorksJobPickup(source)
+	if (source == localPlayer and getElementType(source) == "player" and not isPedInVehicle(source)) then
+		toggleControl("enter_exit",false)
+		triggerServerEvent("WorksJobEnterDoor",localPlayer,localPlayer)
+	end
+end
+addEventHandler("onClientPickupHit",Works_Job_Enter,onClientHitEnterWorksJobPickup)
+
+function WorksJobEnterDoor()
+	triggerServerEvent("WorksJobEnterDoor",localPlayer,localPlayer)
+end
+
+Works_Job_Exit = createPickup(2577.4087,-1289.8916,1044.1250,3,1318,10)
+setElementInterior(Works_Job_Exit,2)
+setElementDimension(Works_Job_Exit,5)
+
+function onClientHitExitWorksJobPickup(source)
+	if (source == localPlayer and getElementType(source) == "player" and not isPedInVehicle(source)) then
+		triggerServerEvent("WorksJobExitDoor",localPlayer,localPlayer)
+	end
+end
+addEventHandler("onClientPickupHit",Works_Job_Exit,onClientHitExitWorksJobPickup)
+
+function onClientLeaveExitWorksJobPickup(source)
+	if (source == localPlayer and getElementType(source) == "player" and not isPedInVehicle(source)) then
+		toggleControl("enter_exit",true)
+		unbindKey("F","down",WorksJobExitDoor)
+	end
+end
+addEventHandler("onClientPickupLeave",Works_Job_Exit,onClientLeaveExitWorksJobPickup)
+
+function WorksJobExitDoor()
+	triggerServerEvent("WorksJobExitDoor",localPlayer,localPlayer)
+end
+
+function renderJobName()
+	if isElement(Works_Job_Enter) then
+		local cx,cy,cz = getCameraMatrix()
+		local px,py,pz = getElementPosition(Works_Job_Enter)
+		local distance = getDistanceBetweenPoints3D(cx,cy,cz,px,py,pz)
+		local posx,posy = getScreenFromWorldPosition(px,py,pz+0.025*distance+0.40)
+		if posx and distance <= 15 then
+			dxDrawBorderedText("المدخل للوظيفه",posx-(0.5),posy-(20),posx-(0.5),posy-(20),tocolor(255,0,0,math.abs(math.sin(getTickCount()/700))*255),1,1, 2-(distance/20),"default-bold","center","top",false,false,false)
+		end
+	end
+end
+addEventHandler("onClientHUDRender",root,renderJobName)
+
+function renderJobNameExit()
+	if isElement(Works_Job_Exit) then
+		local cx,cy,cz = getCameraMatrix()
+		local px,py,pz = getElementPosition(Works_Job_Exit)
+		local distance = getDistanceBetweenPoints3D(cx,cy,cz,px,py,pz)
+		local posx,posy = getScreenFromWorldPosition(px,py,pz+0.025*distance+0.40)
+		if posx and distance <= 15 then
+			dxDrawBorderedText("المخرج لوظيفه السلاح",posx-(0.5),posy-(20),posx-(0.5),posy-(20),tocolor(255,0,0,math.abs(math.sin(getTickCount()/700))*255),1,1, 2-(distance/20),"default-bold","center","top",false,false,false)
+		end
+	end
+end
+addEventHandler("onClientHUDRender",root,renderJobNameExit)
+
+function renderJobName()
+	if isElement(jobpick) then
+		local cx,cy,cz = getCameraMatrix()
+		local px,py,pz = getElementPosition(jobpick)
+		local distance = getDistanceBetweenPoints3D(cx,cy,cz,px,py,pz)
+		local posx,posy = getScreenFromWorldPosition(px,py,pz+0.025*distance+0.40)
+		if posx and distance <= 15 then
+			dxDrawBorderedText("دخول / خروج من الوظيفة",posx-(0.5),posy-(20),posx-(0.5),posy-(20),tocolor(0,255,70,math.abs(math.sin(getTickCount()/700))*255),1,1, 2-(distance/20),"default-bold","center","top",false,false,false)
+		end
+	end
+end
+addEventHandler("onClientHUDRender",root,renderJobName)
+
+function renderJobName()
+	if isElement(cashpick) then
+		local cx,cy,cz = getCameraMatrix()
+		local px,py,pz = getElementPosition(cashpick)
+		local distance = getDistanceBetweenPoints3D(cx,cy,cz,px,py,pz)
+		local posx,posy = getScreenFromWorldPosition(px,py,pz+0.025*distance+0.40)
+		if posx and distance <= 15 then
+			dxDrawBorderedText("بيع الأسلحة المصنوعة",posx-(0.5),posy-(20),posx-(0.5),posy-(20),tocolor(255,0,70,math.abs(math.sin(getTickCount()/700))*255),1,1, 2-(distance/20),"default-bold","center","top",false,false,false)
+		end
+	end
+end
+addEventHandler("onClientHUDRender",root,renderJobName)
+
+function dxDrawBorderedText(text,left,top,right,bottom,color,scale,outlinesize,font,alignX,alignY,clip,wordBreak,postGUI,colorCoded)
+	local outlinesize = math.min(scale,outlinesize)
+	if outlinesize > 0 then
+		for offsetX=-outlinesize,outlinesize,outlinesize do
+			for offsetY=-outlinesize,outlinesize,outlinesize do
+				if not (offsetX == 0 and offsetY == 0) then
+					dxDrawText(text:gsub("#%x%x%x%x%x%x",""), left+offsetX, top+offsetY, right+offsetX, bottom+offsetY, tocolor(0, 0, 0, 255), scale, font, alignX, alignY, clip, wordBreak, postGUI)
+				end
+			end
+		end
+	end
+	dxDrawText(text, left, top, right, bottom, color, scale, font, alignX, alignY, clip, wordBreak, postGUI, colorCoded)
+
+end
---
+++ resources/Weapons/server.lua
@@ -0,0 +1,245 @@
+﻿createBlip (-86.2715,-299.3637,2.7646, 8)
+
+boxes = {}
+obj = {
+	{2578.4,-1289.9513,1044.1250},
+	{2522.9,-1286.0757,1054.6406},
+}
+markers = {
+	{2558.6006,-1291.0045,1043.5},
+	{2556.2268,-1291.0056,1043.5},
+	{2553.8550,-1291.0057,1043.5},
+	{2544.4717,-1291.0048,1043.5},
+	{2542.0872,-1291.0050,1043.5},
+	{2542.0264,-1295.8530,1043.5},
+	{2544.3296,-1295.8505,1043.5},
+	{2553.7854,-1295.8502,1043.5},
+	{2556.2200,-1295.8524,1043.5},
+	{2558.5078,-1295.8507,1043.5},
+}
+jobmarkers = {
+	{2559.1150,-1287.2179,1044.63},
+	{2551.1011,-1287.2179,1044.63},
+	{2543.1372,-1287.2181,1044.63},
+	{2543.0427,-1300.0942,1044.63},
+	{2551.0955,-1300.0951,1044.63},
+	{2559.0603,-1300.0948,1044.63},
+}
+
+checkm = false
+
+centerMarker = createMarker(2565.4836,-1292.9449,1043.15,"cylinder",1.8,255,25,25,150)
+setElementInterior(centerMarker,2)
+setElementDimension(centerMarker,5)
+setElementVisibleTo(centerMarker,root,false)
+
+for i,pos in ipairs(obj) do
+	obj[i] = createObject(3095,pos[1], pos[2], pos[3],0,90,180)
+	setElementInterior(obj[i],2)
+	setElementDimension(obj[i],5)
+end
+
+function onPlayerTakenParts(player,matchingDimension)
+	if getElementType(player) == "player" then
+		if getElementData(player,"worldjob") == true then
+			if not getElementData(player,"part") == true then
+				if not getElementData(player,"weapon") == true then
+					setPedAnimation (player,"WEAPONS","SHP_2H_LIFT",-1,false,false,true,false)
+					giveWeapon(player,25,1)
+					setPedWeaponSlot(player,3)
+					setElementData(player,"part",true)
+					outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : كذا تمام الحين روح للطاوله يلي وراك.",player,200,170,0,true)
+				else
+					outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : يجب أن تحمل أسلحة إلى المستودع، قبل اتخاذ تفاصيل جديدة.",player,200,0,0,true)
+				end
+			else
+				outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : كنت قد اتخذت بالفعل جزءا.",player,200,0,0,true)
+			end
+		end
+	end
+end
+
+for i,pos in ipairs(jobmarkers) do
+	jobmarkers[i] = createMarker(pos[1], pos[2], pos[3],"arrow",0.8,0,0,0,255)
+	setElementInterior(jobmarkers[i],2)
+	setElementDimension(jobmarkers[i],5)
+	addEventHandler("onMarkerHit", jobmarkers[i], onPlayerTakenParts)
+end
+
+function onPlayerStartToCreateWeapon(player,matchingDimension)
+	if getElementType(player) == "player" then
+		if getElementData(player,"worldjob") == true then
+			if not getElementData(player,"weapon") then
+				if getElementData(player,"part") == true then
+					outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : بدأت في صناعة الأسلحة.",player,255,140,0,true)
+					setElementFrozen(player,true)
+					setPedAnimation(player,"BUDDY", "buddy_reload",-1,true,false,true,false)
+					setElementData(player,"part",false)
+					toggleAllControls(player,false)
+					setTimer(function()
+						for i = 1, #markers do
+							if isElementWithinMarker ( player, markers [ i ] ) then
+								checkm = true
+								break
+							end
+						end
+						if checkm == true then
+							if not isElementVisibleTo(centerMarker,player) then
+								setElementVisibleTo(centerMarker,player,true)
+							end
+							setElementFrozen(player,false)
+							--setPedAnimation(player,"CARRY","crry_prtial",1,true,true,false)
+							setPedAnimation (player,"WEAPONS","SHP_2H_LIFT",-1,false, false, false, false)
+							--takeWeapon(player,25)						
+							--boxes = createObject( 1271, 0, 0, 0 );
+							--exports.bone_attach:attachElementToBone( boxes, player, 4, 0, 0.4, - 0.6, -90, 0, 0 );
+							setElementData(player,"weapon",true)
+							toggleAllControls(player,true)
+							toggleControl(player,"fire",false)
+							toggleControl(player,"sprint",false)
+							toggleControl(player,"next_weapon",false)
+							toggleControl(player,"previous_weapon",false)
+							outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : الأسلحة المقدمة، أعتبر إلى المستودع.",player,0,200,0,true)
+						else
+							setPedAnimation(player,false)
+							toggleAllControls(player,true)
+							setElementFrozen(player,false)
+							toggleControl(player,"fire",false)
+							toggleControl(player,"sprint",false)
+							toggleControl(player,"next_weapon",false)
+							toggleControl(player,"previous_weapon",false)
+							outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] :كنت قد ذهبت من مكان العمل!",player,200,0,0,true)
+						end
+					end,10000,1)
+				end
+			end
+		end
+	end
+end
+
+for i,pos in ipairs(markers) do
+	markers[i] = createMarker(pos[1], pos[2], pos[3],"cylinder",0.8,255,0,0,0)
+	setElementInterior(markers[i],2)
+	setElementDimension(markers[i],5)
+	addEventHandler("onMarkerHit", markers[i], onPlayerStartToCreateWeapon)
+end
+
+function onPlayerFinishToCreateWeapon(player,matchingDimension)
+	if source == centerMarker then
+		if getElementType(player) == "player" then
+			if getElementData(player,"worldjob") == true then
+				if getElementData(player,"weapon") == true then
+					local rand = math.random(400,650)
+					if isElementVisibleTo(centerMarker,player) then
+						setElementVisibleTo(centerMarker,player,false)
+					end
+					if getElementData(player,"jobmoney") then
+						setElementData(player,"jobmoney",getElementData(player,"jobmoney")+rand)
+					else
+						setElementData(player,"jobmoney",rand)
+					end
+					setPedAnimation (player,"CARRY","putdwn",-1,false,false,true,false)
+					--setTimer(takeWeapon,600,1,player,25)
+					
+				--	exports.bone_attach:detachElementFromBone(boxes)
+					--destroyElement(boxes)
+				--	boxes[player] = nil
+					takeWeapon(player,25)
+					
+					setElementData(player,"weapon",false)
+					outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] :  #FFFFFF[#00FF00 $"..getElementData(player,"jobmoney").." #FFFFFF] إجمالي المكتسبة  #FFFFFF[#00FF00 $"..rand.." #FFFFFF] تمت صناعة السلاح بننجاح و كان مربوحك منه",player,0,200,0,true)
+				end
+			end
+		end
+	end
+end
+addEventHandler("onMarkerHit",root,onPlayerFinishToCreateWeapon)
+
+function quitJob(player)
+	if player then
+		if getElementData(player,"worldjob") == true then
+			if isElementVisibleTo(centerMarker,player) then
+				setElementVisibleTo(centerMarker,player,false)
+			end
+			toggleControl(player,"fire",true)
+			toggleControl(player,"sprint",true)
+			toggleControl(player,"next_weapon",true)
+			toggleControl(player,"previous_weapon",true)
+			setElementData(player,"worldjob",false)
+			setElementData(player,"part",false)
+			setElementData(player,"weapon",false)
+			setPedAnimation(player,false)
+			takeWeapon(player, 25)
+			outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] : لقد استقال بنجاح. حظا سعيدا. يأتي مرة أخرى.",player,0,150,0,true)
+		end
+	end
+end
+addEvent("onPlayerQuitJob",true)
+addEventHandler("onPlayerQuitJob",root,quitJob)
+
+function giveJobMoney(player)
+	if player then
+		local money = getElementData(player,"jobmoney")
+		if money >= 20 then
+			exports.global:giveMoney(player, money)
+			setElementData(player,"jobmoney",0)
+			outputChatBox("#FF9900*#FFFFFF[#0077FF Weapon #FFFFFF] :  #FFFFFF[#00FF00 $"..money.."#FFFFFF ] راتبك",player,0,200,0,true)
+		end
+	end
+end
+addEvent("giveJobMoney",true)
+addEventHandler("giveJobMoney",root,giveJobMoney)
+
+function quitPlayerOnJob()
+	if getElementData(source,"worldjob") == true then
+		takeWeapon(source,25)
+		giveJobMoney(source)
+		if isElement(boxes[source]) then
+			exports.bone_attach:detachElementFromBone(boxes[source])
+			destroyElement(boxes[source])
+			boxes[source] = nil
+		end
+	end
+end
+addEventHandler("onPlayerQuit",root,quitPlayerOnJob)
+
+function worksJobEnterDoor(player)
+	if not isPedInVehicle(player) then
+		fadeCamera(player,false,2.0,0,0,0)
+		setTimer(setElementPosition,1000,1,player,2575.6145,-1289.7745,1044.1250)
+		setTimer(setElementInterior,1000,1,player,2)
+		setTimer(setElementDimension,1000,1,player,5)
+		setTimer(setCameraTarget,1050,1,player,player)
+		setTimer(fadeCamera,1000,1,player,true)
+		setTimer(toggleControl,1000,1,player,"enter_exit",true)
+	end
+end
+addEvent("WorksJobEnterDoor",true)
+addEventHandler("WorksJobEnterDoor",root,worksJobEnterDoor)
+
+function worksJobExitDoor(player)
+	if not isPedInVehicle(player) then
+		if getElementData(player,"worldjob") == true then
+			outputChatBox("شكرا, للخروج.",player,200,0,0)
+			return
+		end
+		fadeCamera(player,false,2.0,0,0,0)
+		setTimer(setElementPosition,1000,1,player,-86.2826,-300.8545,2.7646)
+		setTimer(setElementInterior,1000,1,player,0)
+		setTimer(setElementDimension,1000,1,player,0)
+		setTimer(setCameraTarget,1050,1,player,player)
+		setTimer(fadeCamera,1000,1,player,true)
+		setTimer(toggleControl,1000,1,player,"enter_exit",true)
+	end
+end
+addEvent("WorksJobExitDoor",true)
+addEventHandler("WorksJobExitDoor",root,worksJobExitDoor)
+
+--[[function checkStart()
+	if getResourceFromName("bone_attach") and getResourceState(getResourceFromName("bone_attach")) == "running" then
+	else
+		outputChatBox("Ресурс bone_attach не запущен!",root,255,0,0)
+		cancelEvent()
+	end
+end
+addEventHandler("onResourceStart",resourceRoot,checkStart)]]
