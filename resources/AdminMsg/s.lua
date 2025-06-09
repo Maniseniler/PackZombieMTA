57b4b331b51c82e9b432d2ba65afdf80e857e72a
Ibrahim Bouchaala <49410570+Maniseniler@users.noreply.github.com>
2025-06-09 09:12:22 +0100

    Add files via upload

---
+++ resources/AdminMsg/s.lua
@@ -0,0 +1,30 @@
+﻿local Groups = { "Console", "AdminMsg" }
+
+function isPlayerAdmin(player)
+    for k,v in ipairs ( Groups ) do
+        if ( isObjectInACLGroup("user." .. getAccountName(getPlayerAccount(player)), aclGetGroup(v)) ) then
+		    return true
+		end
+	end
+end
+
+addCommandHandler("AdminMsg", function ( player, cmd, ... )
+    if not ( isGuestAccount ( getPlayerAccount ( player ) ) ) then
+	    if ( isPlayerAdmin ( player ) ) then
+		    local msg = {...}
+		    local message = table.concat(msg, " ")
+			setElementData ( resourceRoot, "Have_", true )
+			setElementData ( resourceRoot, "Msg_", message )
+			if ( getElementData ( player, "Name" ) and getElementData ( player, "Name" ) ~= false ) then
+				setElementData ( resourceRoot, "By_", getElementData ( player, "Name" ))
+			else
+				setElementData ( resourceRoot, "By_", getPlayerName ( player ))
+			end
+		else
+		    outputChatBox("* You Are Not Admin !", player, 255, 0, 0, true)
+		end
+	else
+	    outputChatBox("* Please Login !", player, 255, 0, 0, true)
+	end
+end)
+
